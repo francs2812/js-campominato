27a2b6079deac328cbfbe2968cf209f4211d8c02
EmilioFrangella <emiliofrangella2812@gmail.com>
2021-05-05 17:57:29 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -3,7 +3,22 @@
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+//chiedi il livello di difficolta
+var levelDif = 0;
+var level = prompt("Digita livello di difficoltà da 0 a 2");
 
+switch(level) {
+    case "0" :
+        var levelDif = 100;
+    break;
+    case "1" :
+        var levelDif = 80;
+    break;
+    case "2" :
+        var levelDif = 50;
+    break;
+}
+console.log(levelDif);
 //verifica numeri nell'arrey
 function scanNumber(element, array) {
     for (var i = 0; i<array.length; i++) {
@@ -18,7 +33,7 @@ function scanNumber(element, array) {
 
 var bombs = [];
     while(bombs.length < 16) {
-        var number = randomNumber(1, 100);
+        var number = randomNumber(1, levelDif);
         //console.log(number)
         if (!scanNumber(number, bombs)){
           bombs.push(number);
